bc62c40129c07f27e0d325f4123d95402294d872
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-17 09:56:46 -0300

    primeiro commit

---
+++ src/main/java/org/serratec/adocao_pets/AdocaoPetsApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.adocao_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoPetsApplication.class, args);
+	}
+
+}
