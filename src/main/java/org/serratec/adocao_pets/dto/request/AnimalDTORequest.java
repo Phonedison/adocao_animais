8304a90fdca5e7719e2d705c446f76a398401696
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-21 12:30:13 -0300

    Adição de validação na classe Request de Animal por causa de erro 500

--- src/main/java/org/serratec/adocao_pets/dto/request/AnimalDTORequest.java
+++ src/main/java/org/serratec/adocao_pets/dto/request/AnimalDTORequest.java
@@ -2,6 +2,7 @@ package org.serratec.adocao_pets.dto.request;
 
 import java.util.List;
 import java.util.Set;
+
 import org.serratec.adocao_pets.domain.Animal;
 import org.serratec.adocao_pets.domain.Caracteristica;
 import org.serratec.adocao_pets.enumerated.Especie;
@@ -9,14 +10,19 @@ import org.serratec.adocao_pets.enumerated.Sexo;
 import org.serratec.adocao_pets.enumerated.StatusAdocao;
 import org.serratec.adocao_pets.enumerated.Tamanho;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public record AnimalDTORequest(
-        String nome,
-        Integer mesesVida,
-        String descricao,
-        Especie especie,
-        Tamanho tamanho,
-        Sexo sexo,
-        StatusAdocao statusAdocao,
+        @NotBlank(message = "O campo não pode estar em branco") String nome,
+        @NotNull(message = "O campo não pode estar nulo") @Positive(message = "Preencha o campo com valores acima de ZERO") Integer mesesVida,
+        @Size(min = 10, max = 100, message = "Campo com permissão de 10 a 100 caracteres") String descricao,
+        @NotNull(message = "A espécie deve ser informada, informe como CACHORRO ou GATO") Especie especie,
+        @NotNull(message = "O tamanho deve ser informado como PEQUENO, MEDIO ou GRANDE") Tamanho tamanho,
+        @NotNull(message = "O sexo deve ser informado como MACHO ou FEMEA") Sexo sexo,
+        @NotNull(message = "O status de adoção deve ser informado como PENDENTE, APROVADO, REJEITADO ou CANCELADO") StatusAdocao statusAdocao,
         List<Long> caracteristicas) {
 
     public Animal toAnimal(Set<Caracteristica> caracteristicasVinculadas) {
