54657abc766f6db81c444099ad5ab73f24dd3b8b
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-21 17:12:09 -0300

    Add schema do swagger na classe ErroResposta

--- src/main/java/org/serratec/adocao_pets/exception/ErroResposta.java
+++ src/main/java/org/serratec/adocao_pets/exception/ErroResposta.java
@@ -3,6 +3,7 @@ package org.serratec.adocao_pets.exception;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Estrutura padrão de retorno para erros e exceções da API")
 public class ErroResposta {
+
+    @Schema(description = "Código do status HTTP", example = "404")
     private Integer status;
+
+    @Schema(description = "Mensagem amigável sobre o erro", example = "Recurso não encontrado!")
     private String titulo;
+
+    @Schema(description = "Data e hora em que o erro ocorreu")
     private LocalDateTime dataHora;
+
+    @Schema(description = "Lista detalhada de cada erro encontrado")
     private List<String> erros;
 
 }
